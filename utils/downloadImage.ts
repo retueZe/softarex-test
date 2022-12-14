4ca866c7128d1fc17f7b5ad045c348578906443a
retueZe <84510709+retueZe@users.noreply.github.com>
2022-12-14 14:29:37 +0300

    Upgrade image downloading

--- utils/downloadImage.ts
+++ utils/downloadImage.ts
@@ -1,21 +1,21 @@
-export function downloadImage(url: string): Promise<void> {
-    return fetch(url, {
-            headers: new Headers({
-                'Origin': location.origin
-            }),
-            mode: 'cors'
-        })
-        .then(response => response.blob())
-        .then(blob => {
-            const blobUrl = URL.createObjectURL(blob)
-            forceDownload(blobUrl)
-            URL.revokeObjectURL(blobUrl)
-        })
+export async function downloadImage(url: string): Promise<void> {
+    const response = await fetch(url, {
+        headers: new Headers({
+            'Origin': location.origin
+        }),
+        mode: 'cors'
+    })
+    const blob = await response.blob()
+    const blobUrl = URL.createObjectURL(blob)
+    const pathSegments = new URL(url).pathname.split('/')
+    const fileName = pathSegments[pathSegments.length - 1]
+    forceDownload(blobUrl, fileName)
+    URL.revokeObjectURL(blobUrl)
 }
-function forceDownload(url: string) {
+function forceDownload(url: string, fileName: string) {
     const a = document.createElement('a')
     a.href = url
-    a.download = ''
+    a.download = fileName
     document.body.appendChild(a)
     a.click()
     a.remove()
